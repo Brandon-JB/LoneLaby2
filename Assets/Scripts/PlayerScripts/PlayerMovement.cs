19163af18f1d1c27ab2f3e1dc26ac376e4e20df4
AlexanderOr <aortwine@ltu.edu>
2025-01-23 14:39:41 -0500

    Basic movement (not grid based)

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -5,18 +5,50 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private InputActionReference Movement, Attack;
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
+
+    [SerializeField] private InputActionReference Movement, Attack;
+
+    [SerializeField] private float MoveSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            movementInput = new Vector2(0, Input.GetAxis("Vertical"));
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            movementInput = new Vector2(0, Input.GetAxis("Vertical"));
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            movementInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+            rb.velocity = movementInput * MoveSpeed;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            movementInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+            rb.velocity = movementInput * MoveSpeed;
+        }
+        else
+        {
+            rb.velocity = movementInput * 0;
+
+        }
+
     }
 }
