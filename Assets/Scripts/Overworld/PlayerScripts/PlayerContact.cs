b559d0268367b4ca2ece3315a4d372b6540157c8
AlexanderOr <aortwine@ltu.edu>
2025-01-28 15:45:52 -0500

    Portals
    Sends player to combat

--- Assets/Scripts/Overworld/PlayerScripts/PlayerContact.cs
+++ Assets/Scripts/Overworld/PlayerScripts/PlayerContact.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class PlayerContact : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public PortalScript portalScript;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Portal")
+        {
+            portalScript.TeleportPlayer();
+        }
+
+        //portalScript.TeleportPlayer();
     }
 }
