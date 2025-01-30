a1336cdb7a23f5643a167e18b65cde4b7031d284
AlexanderOr <aortwine@ltu.edu>
2025-01-30 13:32:55 -0500

    build1 part 2

---
+++ Assets/Scripts/Overworld/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseCanvas;
+    public bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        { 
+            if (isPaused == false)
+            {
+                PauseCanvas.SetActive(true);
+                isPaused = true;
+                Time.timeScale = 0;
+            }
+            else if (isPaused == true)
+            {
+                PauseCanvas.SetActive(false);
+                isPaused = false;
+                Time.timeScale = 1;
+            }
+            
+        }
+
+
+    }
+}
