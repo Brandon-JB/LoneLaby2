b3179cb812ccbb1b35d64c27ca4f48984d2e17c1
AlexanderOr <aortwine@ltu.edu>
2025-02-04 15:01:37 -0500

    Grid based done

--- Assets/Scripts/Overworld/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/Overworld/PlayerScripts/PlayerMovement.cs
@@ -8,27 +8,75 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementInput;
     private Rigidbody2D rb;
 
-    [SerializeField] private InputActionReference Movement, Attack;
+    public float MoveSpeed = 5f;
 
-    [SerializeField] private float MoveSpeed = 5f;
 
-    private PlayerActions playAction;
+    public Animator animator;
 
-    private Animator animator;
+    //grid based movement
+    public Transform movePoint;
+    public LayerMask SolidObjects;
+    public float Timer = 1;
+    public bool CanWalk = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-
-        playAction = GetComponent<PlayerActions>();
+        //animator = GetComponent<Animator>();
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Movement...looks so bad
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, MoveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, SolidObjects ))
+                {
+                    if(CanWalk)
+                    {
+                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                        animator.SetFloat("x", Input.GetAxisRaw("Horizontal"));
+                        animator.SetFloat("y", 0);
+                        animator.SetBool("isWalking", true);
+                    }
+                }
+            } 
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, SolidObjects))
+                {
+                    if(CanWalk)
+                    {
+                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                        animator.SetFloat("x", 0);
+                        animator.SetFloat("y", Input.GetAxisRaw("Vertical"));
+                        animator.SetBool("isWalking", true);
+                    }
+                }
+            }
+            else
+            {
+                animator.SetBool("isWalking", false);
+            }
+        }
+
+
+
+    }
+
+
+
+}
+
+
+
+/*//Movement...looks so bad
         movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         if (Input.GetAxis("Vertical") > 0)
         {
@@ -71,7 +119,4 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = movementInput * 0;
             animator.SetBool("isWalking", false);
 
-        }
-
-    }
-}
+        }*/
