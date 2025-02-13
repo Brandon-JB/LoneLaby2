f54b82faac7c04e26f8be0dc5e69e12d00de7730
AlexanderOr <aortwine@ltu.edu>
2025-02-13 12:13:00 -0500

    Build 2 final

--- Assets/Scripts/Combat/CombatMap/CombatPortals.cs
+++ Assets/Scripts/Combat/CombatMap/CombatPortals.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CombatPortals : MonoBehaviour
 {
 
-    public GameObject Portal;
-    public GameObject Destination;
+    public GameObject[] Portal;
+    public GameObject[] Destination;
+    public GameObject Player;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +21,34 @@ public class CombatPortals : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void TeleportPlayer(string name)
+    {
+        Debug.Log(name);
+        if (name == "Floor1-2")
+        {
+            Player.transform.position = Destination[0].transform.position;
+        }
+
+        if (name == "Floor2-1")
+        {
+            Player.transform.position = Destination[1].transform.position;
+        }
+
+        if (name == "Floor2-3")
+        {
+            Player.transform.position = Destination[2].transform.position;
+        }
+
+        if (name == "Floor3-2")
+        {
+            Player.transform.position = Destination[3].transform.position;
+        }
+
+        if (name == "Exit")
+        {
+            SceneManager.LoadScene("NoCombatAreas");
+        }
     }
 }
