fc97627a06733005c2347a938038a2329c28b606
Katherine P. Tackett <ktackett@ltu.edu>
2025-02-28 01:27:43 -0500

    updated adrknes

--- Assets/Scripts/Combat/UI/DarknessManager.cs
+++ Assets/Scripts/Combat/UI/DarknessManager.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class DarknessManager : MonoBehaviour
 {
-    public Image DarknessImage;
-    public Sprite[] sprites;
+    public CanvasGroup DarknessImage;
     public float ChangeTime = 30f;
     public float timer = 0f;
 
@@ -15,24 +15,30 @@ public class DarknessManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DarknessImage.enabled = true;
+        DarknessImage.DOFade(1, 30f);
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= ChangeTime) 
-        {
-            timer = 0f;
-            currentIndex++;
-            DarknessImage.sprite = sprites[currentIndex];
-        }
+        //timer += Time.deltaTime;
+        //if (timer >= ChangeTime) 
+        //{
+        //    timer = 0f;
+        //    currentIndex++;
+        //    DarknessImage.sprite = sprites[currentIndex];
+        //}
 
         if(InputManager.magicPressed)
         {
             currentIndex = 0;
             timer = 0f;
-            DarknessImage.sprite = sprites[currentIndex];
+            DarknessImage.DOKill();
+            DarknessImage.DOFade(0, 1f).OnComplete(() =>
+            {
+                DarknessImage.DOFade(1, 30f);
+            });
+            //DarknessImage.sprite = sprites[currentIndex];
         }
     }
 
