46ba228461c2852eb24bbbd12dd6c0747247dd8c
Brandon-JB <bbourdage@ltu.edu>
2025-03-10 18:13:59 -0400

    Started work on Viin

---
+++ Assets/Scripts/Combat/StatScripts/Bosses/ViinChar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViinChar : BaseChar
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        charName = "Viin";
+        allied = false;
+    }
+}
