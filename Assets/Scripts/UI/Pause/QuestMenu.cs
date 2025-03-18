22a55c26cb9fc9a4b13d4a17753c9ab1a160bbde
Katherine P. Tackett <ktackett@ltu.edu>
2025-03-18 15:32:04 -0400

    quest menu looks better

--- Assets/Scripts/UI/Pause/QuestMenu.cs
+++ Assets/Scripts/UI/Pause/QuestMenu.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class QuestMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Eventually this will change based on whose dead and whose alive but thats not important rn
+
+    [SerializeField] private Transform wholeQuestMenu;
+    [SerializeField] private Transform[] locations;
+    //This will move the back button. Yuck. Maybe turn it off for a minute to turn it back on after the side quests come up?
+    //whatever im gonna try this and see
+
+    public void GoToSideQuest()
     {
-        
+        wholeQuestMenu.DOMove(locations[1].position, 1f);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToCoreQuests()
     {
-        
+        wholeQuestMenu.DOMove(locations[0].position, 1f);
     }
 }
