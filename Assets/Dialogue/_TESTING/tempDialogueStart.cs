3666983f2769eef73973485adc5d2a5f6517515d
Katherine P. Tackett <ktackett@ltu.edu>
2025-03-30 21:29:01 -0400

    imported all dialogue (thank god)

--- Assets/Dialogue/_TESTING/tempDialogueStart.cs
+++ Assets/Dialogue/_TESTING/tempDialogueStart.cs
@@ -6,9 +6,11 @@ public class tempDialogueStart : MonoBehaviour
 {
     [SerializeField] mainDialogueManager MDM;
 
+    [SerializeField] private string fileName = "introducingSuspects";
+
     private void Start()
     {
         Debug.Log("I am in the dialogue scene");
-        MDM.dialogueSTART("introducingSuspects");
+        MDM.dialogueSTART(fileName);
     }
 }
