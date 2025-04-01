a6b3ab62630fcbe3e28339d3aca61a875739c807
AlexanderOr <aortwine@ltu.edu>
2025-04-01 16:35:10 -0400

    Save Point Done

---
+++ Assets/Scripts/NonCombat/SavePoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour
+{
+    public GameObject Player;
+    public saveUI saveui;
+    private float maxDistance = 1.5f;
+    private float DistanceBetweenObjects;
+    private float InteractionLength = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceBetweenObjects = Vector3.Distance(transform.position, Player.transform.position);
+        if (DistanceBetweenObjects <= maxDistance)
+        {
+            if (InputManager.interactPressed == true)
+            {
+                saveui.OPENSAVEMENU();
+            }
+        }
+    }
+}
