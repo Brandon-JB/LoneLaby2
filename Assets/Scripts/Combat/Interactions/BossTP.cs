02f307558fb50cab10cc9fe72bcd822d94c83251
Brandon-JB <bbourdage@ltu.edu>
2025-04-01 15:55:57 -0400

    Added bossTP sign

---
+++ Assets/Scripts/Combat/Interactions/BossTP.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTP : CombatInteraction
+{
+    public GameObject bossTP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+
+        if (DistanceBetweenObjectAndPlayer <= interactRange)
+        {
+            if (InputManager.interactPressed)
+            {
+                Player.transform.position = bossTP.transform.position;
+            }
+        }
+    }
+}
