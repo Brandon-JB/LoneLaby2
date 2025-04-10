a3da2224bf49aba58cb2364eb783b1a87dda69c3
Katherine P. Tackett <ktackett@ltu.edu>
2025-04-10 12:11:27 -0400

    added options to main menu, updated tutorial again

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -9,12 +9,24 @@ public class MainMenuScript : MonoBehaviour
     public mainDialogueManager mainDialogueManager;
     public CanvasGroup bg;
 
+    [SerializeField]private Transform[] optionsLoc;
+
     private void Start()
     {
         Time.timeScale = 1.0f;
         bg.alpha = 1.0f;
         bg.DOFade(0, 1f).SetUpdate(true);
         //audioManager.Instance.playBGM("T1");
+    }
+
+    public void goToOptions()
+    {
+        optionsLoc[0].DOMove(optionsLoc[1].position, 1f);
+    }
+
+    public void exitOptions()
+    {
+        optionsLoc[0].DOMove(optionsLoc[2].position, 1f);
     }
 
     public void tempTeleportToGame() { Time.timeScale = 1f; SceneManager.LoadScene("NoCombatAreas"); }
