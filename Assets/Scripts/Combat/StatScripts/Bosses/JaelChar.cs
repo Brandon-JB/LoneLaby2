afd1e92153daa1c6b9cf2339f96cc90b8f053689
Brandon-JB <bbourdage@ltu.edu>
2025-04-10 13:33:35 -0400

    Started work on Jael boss fight

---
+++ Assets/Scripts/Combat/StatScripts/Bosses/JaelChar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaelChar : BaseChar
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        charName = "Jael";
+        allied = false;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+}
