607e82c27b83bc25c3b2e52b2bf4db6a272fd5ff
Katherine P. Tackett <ktackett@ltu.edu>
2025-04-11 09:24:24 -0400

    tried putting a bandaid over some of our issues for the playtest

---
+++ Assets/Scripts/UI/slidersForNoCombat.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slidersForNoCombat : MonoBehaviour
+{
+
+    //COME BACK AND EDIT THIS
+
+    //[SerializeField] private BaseChar character;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    character.ChangeSpecificStat("Health", character.GetHealth());
+    //    character.ChangeSpecificStat("Mana", character.GetMana());
+    //}
+}
