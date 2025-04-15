e7abc3287e0433e1313c3f5a4b67b10e583328b0
AlexanderOr <aortwine@ltu.edu>
2025-04-15 15:22:20 -0400

    Everything

--- Assets/Scripts/Overworld/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/Overworld/PlayerScripts/PlayerMovement.cs
@@ -30,40 +30,48 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, MoveSpeed * Time.deltaTime);
-
-        if(Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Input.GetAxisRaw("Vertical") == 1)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, SolidObjects ))
-                {
-                    if(CanWalk)
-                    {
-                        movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                        animator.SetFloat("x", Input.GetAxisRaw("Horizontal"));
-                        animator.SetFloat("y", 0);
-                        animator.SetBool("isWalking", true);
-                    }
-                }
-            } 
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-            {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, SolidObjects))
-                {
-                    if(CanWalk)
-                    {
-                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                        animator.SetFloat("x", 0);
-                        animator.SetFloat("y", Input.GetAxisRaw("Vertical"));
-                        animator.SetBool("isWalking", true);
-                    }
-                }
-            }
-            else
-            {
-                animator.SetBool("isWalking", false);
-            }
+            movementInput = new Vector2(0, Input.GetAxisRaw("Vertical"));
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+            animator.SetFloat("x", 0);
+            animator.SetFloat("y", 1);
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetAxisRaw("Vertical") == -1)
+        {
+            movementInput = new Vector2(0, Input.GetAxisRaw("Vertical"));
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+            animator.SetFloat("x", 0);
+            animator.SetFloat("y", -1);
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+            animator.SetFloat("y", 0);
+            animator.SetFloat("x", 1);
+            animator.SetBool("isWalking", true);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+            rb.velocity = movementInput * MoveSpeed;
+            //animations
+            animator.SetFloat("y", 0);
+            animator.SetFloat("x", -1);
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            rb.velocity = movementInput * 0;
+            animator.SetBool("isWalking", false);
+
         }
 
 
