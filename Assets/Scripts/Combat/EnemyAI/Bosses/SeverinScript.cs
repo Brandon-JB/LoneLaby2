61c381bfab755d7a60728b8f910a1c1249c6bc84
Brandon-JB <bbourdage@ltu.edu>
2025-04-22 16:38:23 -0400

    Started work on changing severin

--- Assets/Scripts/Combat/EnemyAI/Bosses/SeverinScript.cs
+++ Assets/Scripts/Combat/EnemyAI/Bosses/SeverinScript.cs
@@ -40,9 +40,14 @@ public class SeverinScript : EnemyScript
     /*Severin Combat loop:
      * 1. Follow player around like a normal enemy and attack
      * 2. At 50/25%, start the big attack
+     *      a. Have a list of possible dash positions spread throughout the arena
+     *      b. Have Severin choose the farthest one from Leora to dash to
+     *      c. Dash to that position (with hurtbox turned off to prevent pushing)
+     *      d. start windup
      * 3. If the player hits Severin during the windup, then she does the charged attack early and it's unparriable
-     * 4. After attack charges up, severin does the big attack which needs to be parried
-     * 5. After this, severin gets stunned for several seconds
+     * 4. After attack charges up, severin dashes towards the player
+     * 5. After severin reaches the player, she does a massive attack that covers the whole screen and needs to be parried
+     * 6. After this, severin gets stunned for several seconds
     */
 
     // Update is called once per frame
@@ -83,11 +88,175 @@ public class SeverinScript : EnemyScript
         //Movement
         if (canMove == true && enemyChar.animator.GetBool("Attacking") == false)
         {
-            enemyRB.velocity = Vector2.zero;
-
-            DistanceFromPlayer = Vector2.Distance(this.transform.position, Player.transform.position);
-            if ((DistanceFromPlayer <= followRange && DistanceFromPlayer > attackRange) /*&& (PlayerController.isfrozen == false)*/)
+            //Not moving away to prepare for the massive attack
+            if (!enemyChar.animator.GetBool("dashAway")) 
             {
+                enemyRB.velocity = Vector2.zero;
+
+                DistanceFromPlayer = Vector2.Distance(this.transform.position, Player.transform.position);
+                if ((DistanceFromPlayer <= followRange && DistanceFromPlayer > attackRange) /*&& (PlayerController.isfrozen == false)*/)
+                {
+                    path.maxSpeed = moveSpeed;
+
+                    path.destination = PlayerRB.transform.position;
+
+                    //enemyRB.transform.position = Vector2.MoveTowards(enemyRB.transform.position, PlayerRB.transform.position, moveSpeed * Time.deltaTime);
+
+
+                    //EnemyRB.transform.position = Vector2.MoveTowards(EnemyRB.transform.position, PlayerRB.transform.position, Speed * Time.deltaTime);
+
+                    //Animations
+                    enemyChar.animator.SetBool("isMoving", true);
+
+                    #region Directional Animating
+                    
+
+                    float xDistance = Mathf.Abs(Mathf.Abs(this.transform.position.x) - Mathf.Abs(Player.transform.position.x));
+                    float yDistance = Mathf.Abs(Mathf.Abs(this.transform.position.y) - Mathf.Abs(Player.transform.position.y));
+
+                    //if the enemy is to the right of the player.
+                    if (this.transform.position.x > Player.transform.position.x)
+                    {
+                        enemyChar.animator.SetFloat("moveX", -1);
+                    }
+                    else //enemy is to the left of the player
+                    {
+                        enemyChar.animator.SetFloat("moveX", 1);
+                    }
+
+                    //if the enemy is above the player.
+                    if (this.transform.position.y > Player.transform.position.y)
+                    {
+                        enemyChar.animator.SetFloat("moveY", -1);
+                    }
+                    else //enemy is below the player
+                    {
+                        enemyChar.animator.SetFloat("moveY", 1);
+                    }
+
+                    //Seeing whether the enemy is closer on the x or y coordinate
+                    //Need to figure out a better way of doing this
+                    if (xDistance > yDistance)
+                    {
+                        enemyChar.animator.SetFloat("moveY", 0);
+                    }
+                    else if (xDistance < yDistance)
+                    {
+                        enemyChar.animator.SetFloat("moveX", 0);
+                    }
+                    else //if the distances are the same
+                    {
+                        Debug.Log("X and y distances are the same");
+                    }
+                    #endregion
+                }
+                //Attacking
+                else if (DistanceFromPlayer <= attackRange)
+                {
+                    if (cooldown.isCoolingDown) return;
+
+
+
+                    canMove = false;
+
+                    #region Somehow works
+                    //Getting the distances between the x and y coordinates
+                    float xDistance = Mathf.Abs(Mathf.Abs(this.transform.position.x) - Mathf.Abs(Player.transform.position.x));
+                    float yDistance = Mathf.Abs(Mathf.Abs(this.transform.position.y) - Mathf.Abs(Player.transform.position.y));
+
+                    //if the enemy is to the right of the player.
+                    if (this.transform.position.x > Player.transform.position.x)
+                    {
+                        enemyChar.animator.SetFloat("moveX", -1);
+                    }
+                    else //enemy is to the left of the player
+                    {
+                        enemyChar.animator.SetFloat("moveX", 1);
+                    }
+
+                    //if the enemy is above the player.
+                    if (this.transform.position.y > Player.transform.position.y)
+                    {
+                        enemyChar.animator.SetFloat("moveY", -1);
+                    }
+                    else //enemy is below the player
+                    {
+                        enemyChar.animator.SetFloat("moveY", 1);
+                    }
+
+                    //Seeing whether the enemy is closer on the x or y coordinate
+                    //Need to figure out a better way of doing this
+                    if (xDistance > yDistance)
+                    {
+                        enemyChar.animator.SetFloat("moveY", 0);
+                    }
+                    else if (xDistance < yDistance)
+                    {
+                        enemyChar.animator.SetFloat("moveX", 0);
+                    }
+                    else //if the distances are the same
+                    {
+                        Debug.Log("X and y distances are the same");
+                    }
+
+                    //if the player is horizontal to dark leora
+                    if (enemyChar.animator.GetFloat("moveX") != 0)
+                    {
+                        if (yDistance > 1)
+                        {
+                            Vector2 movePostion = new Vector2(this.transform.position.x, PlayerRB.transform.position.y);
+
+                            path.maxSpeed = moveSpeed;
+
+                            path.destination = movePostion;
+
+
+                            //enemyRB.transform.position = Vector2.MoveTowards(enemyRB.transform.position, movePostion, moveSpeed * Time.deltaTime);
+
+                            return;
+                        }
+                    }
+                    //if the player is vertical to dark leora
+                    else if (enemyChar.animator.GetFloat("moveY") != 0)
+                    {
+                        if (xDistance > 1)
+                        {
+                            Vector2 movePostion = new Vector2(PlayerRB.transform.position.x, this.transform.position.y);
+
+                            path.maxSpeed = moveSpeed;
+
+                            path.destination = movePostion;
+
+                            //enemyRB.transform.position = Vector2.MoveTowards(enemyRB.transform.position, movePostion, moveSpeed * Time.deltaTime);
+
+                            return;
+                        }
+                    }
+
+                    #endregion
+
+                    enemyRB.velocity = Vector2.zero;
+                    path.maxSpeed = 0;
+
+                    path.destination = this.transform.position;
+
+                    enemyChar.animator.SetBool("Attacking", true);
+
+                    cooldown.cooldownTime = defaultCooldown;
+                    cooldown.StartCooldown();
+
+                }
+            }
+            else //moving away to do the massive attack
+            {
+                //
+                //
+                //
+                //
+                //
+                //
+                //Look at steps at the top for plans for this
+
                 path.maxSpeed = moveSpeed;
 
                 path.destination = PlayerRB.transform.position;
@@ -101,35 +270,7 @@ public class SeverinScript : EnemyScript
                 enemyChar.animator.SetBool("isMoving", true);
 
                 #region Directional Animating
-                /*
-                if (Player.transform.position.x > transform.position.x)
-                {
-                    movementInput.x = 1;
-                }
-                else
-                {
-                    movementInput.x = -1;
-                }
 
-                if (Player.transform.position.y > transform.position.y)
-                {
-                    movementInput.y = 1;
-                }
-                else
-                {
-                    movementInput.y = -1;
-                }
-
-                
-                if (movementInput != Vector2.zero)
-                {
-                    enemyChar.animator.SetFloat("moveX", movementInput.x);
-                    enemyChar.animator.SetFloat("moveY", movementInput.y);
-                }
-
-                enemyChar.animator.SetFloat("moveX", movementInput.x);
-                enemyChar.animator.SetFloat("moveY", movementInput.y);
-                */
 
                 float xDistance = Mathf.Abs(Mathf.Abs(this.transform.position.x) - Mathf.Abs(Player.transform.position.x));
                 float yDistance = Mathf.Abs(Mathf.Abs(this.transform.position.y) - Mathf.Abs(Player.transform.position.y));
@@ -168,103 +309,8 @@ public class SeverinScript : EnemyScript
                 {
                     Debug.Log("X and y distances are the same");
                 }
-                #endregion
-            }
-            //Attacking
-            else if (DistanceFromPlayer <= attackRange)
-            {
-                if (cooldown.isCoolingDown) return;
-
-
-
-                canMove = false;
-
-                #region Somehow works
-                //Getting the distances between the x and y coordinates
-                float xDistance = Mathf.Abs(Mathf.Abs(this.transform.position.x) - Mathf.Abs(Player.transform.position.x));
-                float yDistance = Mathf.Abs(Mathf.Abs(this.transform.position.y) - Mathf.Abs(Player.transform.position.y));
-
-                //if the enemy is to the right of the player.
-                if (this.transform.position.x > Player.transform.position.x)
-                {
-                    enemyChar.animator.SetFloat("moveX", -1);
-                }
-                else //enemy is to the left of the player
-                {
-                    enemyChar.animator.SetFloat("moveX", 1);
-                }
-
-                //if the enemy is above the player.
-                if (this.transform.position.y > Player.transform.position.y)
-                {
-                    enemyChar.animator.SetFloat("moveY", -1);
-                }
-                else //enemy is below the player
-                {
-                    enemyChar.animator.SetFloat("moveY", 1);
-                }
-
-                //Seeing whether the enemy is closer on the x or y coordinate
-                //Need to figure out a better way of doing this
-                if (xDistance > yDistance)
-                {
-                    enemyChar.animator.SetFloat("moveY", 0);
-                }
-                else if (xDistance < yDistance)
-                {
-                    enemyChar.animator.SetFloat("moveX", 0);
-                }
-                else //if the distances are the same
-                {
-                    Debug.Log("X and y distances are the same");
-                }
-
-                //if the player is horizontal to dark leora
-                if (enemyChar.animator.GetFloat("moveX") != 0)
-                {
-                    if (yDistance > 1)
-                    {
-                        Vector2 movePostion = new Vector2(this.transform.position.x, PlayerRB.transform.position.y);
-
-                        path.maxSpeed = moveSpeed;
-
-                        path.destination = movePostion;
-
-
-                        //enemyRB.transform.position = Vector2.MoveTowards(enemyRB.transform.position, movePostion, moveSpeed * Time.deltaTime);
-
-                        return;
-                    }
-                }
-                //if the player is vertical to dark leora
-                else if (enemyChar.animator.GetFloat("moveY") != 0)
-                {
-                    if (xDistance > 1)
-                    {
-                        Vector2 movePostion = new Vector2(PlayerRB.transform.position.x, this.transform.position.y);
-
-                        path.maxSpeed = moveSpeed;
-
-                        path.destination = movePostion;
-
-                        //enemyRB.transform.position = Vector2.MoveTowards(enemyRB.transform.position, movePostion, moveSpeed * Time.deltaTime);
-
-                        return;
-                    }
-                }
 
                 #endregion
-
-                enemyRB.velocity = Vector2.zero;
-                path.maxSpeed = 0;
-
-                path.destination = this.transform.position;
-
-                enemyChar.animator.SetBool("Attacking", true);
-
-                cooldown.cooldownTime = defaultCooldown;
-                cooldown.StartCooldown();
-
             }
         }
     }
