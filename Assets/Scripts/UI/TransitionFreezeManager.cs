1008dd4b909412e369e502d4a628416a604e38da
AlexanderOr <aortwine@ltu.edu>
2025-04-24 15:32:47 -0400

    freeze/unfreeze

---
+++ Assets/Scripts/UI/TransitionFreezeManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionFreezeManager : MonoBehaviour
+{
+    
+    public void FreezeTime()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnFreezeTime()
+    {
+        Time.timeScale = 0;
+    }
+}
