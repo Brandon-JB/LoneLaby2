d2744652795adb28e204f48c3d86414bd4dd360b
Katherine P. Tackett <ktackett@ltu.edu>
2025-04-29 22:41:40 -0400

    added unobtained item descriptions

--- Assets/Scripts/UI/Pause/itemDescOnHover.cs
+++ Assets/Scripts/UI/Pause/itemDescOnHover.cs
@@ -23,6 +23,8 @@ public class itemDescOnHover : MonoBehaviour
     [SerializeField] private string[] allItemNames;
     [SerializeField] private string[] allItemDescs;
 
+    [SerializeField] private string[] unobtainedItemDesc;
+
     public void TriggerWithDelay(string itemName)
     {
         closeItemUI(itemName);
@@ -44,7 +46,7 @@ public class itemDescOnHover : MonoBehaviour
             //change UI text
             itemNameTXT.text = "???";
             //change ui desc
-            itemDescTXT.text = "";
+            itemDescTXT.text = unobtainedItemDesc[keyForArrays];
         } else
         {
             //change UI text
