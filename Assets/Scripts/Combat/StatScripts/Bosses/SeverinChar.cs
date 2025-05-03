2581a5299ad6ef91e8af927add10cd3575a3af8a
Brandon-JB <bbourdage@ltu.edu>
2025-05-02 20:49:38 -0400

    Nerfed severin's health

--- Assets/Scripts/Combat/StatScripts/Bosses/SeverinChar.cs
+++ Assets/Scripts/Combat/StatScripts/Bosses/SeverinChar.cs
@@ -12,7 +12,7 @@ public class SeverinChar : BaseChar
         charName = "Severin";
         allied = false;
 
-        ChangeStats(13, 0, 8, 550, 0);
+        ChangeStats(13, 0, 8, 400, 0);
     }
 
     // Update is called once per frame
